6476c1eb640b7f829c89758a8f728f748d0cb7bd
Gabriel Gaspar <gabrielssgaspar@gmail.com>
2015-11-14 20:12:47 -0200

    Aula 11 - BONUS: diretivas que buscam dados

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,4 +1,4 @@
-angular.module('minhasDiretivas', [])
+angular.module('minhasDiretivas', ['meusServicos'])
 
 .directive('meuPainel', function() {
 
@@ -59,6 +59,31 @@ angular.module('minhasDiretivas', [])
 
 		scope.$on('fotoCadastrada', function() {
 			element[0].focus();
+		});
+
+	};
+
+	return ddo;
+})
+
+.directive('meusTitulos', function() {
+
+	var ddo = {};
+
+	ddo.restrict = 'E';
+
+	ddo.template = '<ul><li ng-repeat="titulo in titulos">{{titulo}}</li></ul>'
+
+	ddo.controller = function($scope, recursoFoto) {
+
+		recursoFoto.query(function(fotos) {
+
+			$scope.titulos = fotos.map(function(foto){
+				return foto.titulo;
+			});
+
+		}, function(erro) {
+			console.log(erro);
 		});
 
 	};
